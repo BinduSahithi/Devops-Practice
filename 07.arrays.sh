a8ce9989c412b829ef33c3979a737d4895956d23
BinduSahithi <bindusahithi20@gmail.com>
2026-06-23 06:07:01 +0530

    added

---
+++ 07.arrays.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+NAMES=("bindu" "sahithi" "sweety")
+
+echo "First name: $NAMES[0]"
+echo "Second name: ${NAMES[1]}"
+echo "Third name: ${NAMES[2]}"
+
+echo "All Names are: ${NAMES[@]}"
